0009c87d519716f4b78d8655ec2822c8f2db162c
thomas4190 <thomas4190@hotmail.com>
2016-11-11 14:12:59 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,25 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+##Basically this function creates a matrix that is able to cache it's inverse values
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) 
+{ v <- NULL
+  s <- function(y) 
+          {x<<-y
+           v<<-NULL}
+ harambe <- function() x
+         HK <- function(mc) v<<-mc
+         HI <- function() v
+         list(s=s, harambe=harambe, HK=HK, HI=HI)}
 
-}
-
-
-## Write a short comment describing this function
-
+## This shows the inverse of the matrix from the above function
+ 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+  v <- x$HI()  
+        if(!is.null(v)) 
+        {return(v)}
+        this <- x$harambe()    
+        v <- solve(this)    
+        x$HK(v)    
+        v } 
